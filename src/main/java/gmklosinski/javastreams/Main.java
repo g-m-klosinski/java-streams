4a518da1959b815917709321b5f59b29d904d68f
Grzegorz Klosinski <g.m.klosinski.wut@gmail.com>
2026-09-22 17:50:22 +0200

    Add tagging parts of speech

--- src/main/java/gmklosinski/javastreams/Main.java
+++ src/main/java/gmklosinski/javastreams/Main.java
@@ -1,7 +1,13 @@
 package gmklosinski.javastreams;
 
+import edu.stanford.nlp.simple.Sentence;
+
 public class Main {
     static void main() {
+        var sentence = new Sentence("I like volleyball!");
 
+        var volleyballPosTag = sentence.posTag(2);
+
+        IO.println("Volleyball is " + volleyballPosTag);
     }
 }
