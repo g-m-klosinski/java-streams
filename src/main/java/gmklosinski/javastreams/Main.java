64829bf359c59c4a7ca4661fed4e1e9e719200b7
Grzegorz Klosinski <g.m.klosinski.wut@gmail.com>
2026-09-23 21:34:33 +0200

    Refactor

--- src/main/java/gmklosinski/javastreams/Main.java
+++ src/main/java/gmklosinski/javastreams/Main.java
@@ -8,12 +8,12 @@ public class Main {
     static void main() throws IOException {
         var document = new Document(new String(System.in.readAllBytes()));
 
-        var firstWord = getFirstWord(document);
+        var summary = sumUp(document);
 
-        IO.println(firstWord);
+        IO.println(summary);
     }
 
-    private static String getFirstWord(Document document) {
+    private static String sumUp(Document document) {
         return document.sentences().getFirst().words().getFirst();
     }
 }
